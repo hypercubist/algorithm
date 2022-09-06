2c7cb68f22d833628e99d6b0594e9dff8a396ba4
hypercubist <58693701+hypercubist@users.noreply.github.com>
2022-09-06 16:41:54 +0900

    백준 9/6

--- back_tracking/bj9663.py
+++ back_tracking/bj9663.py
@@ -1,24 +1,30 @@
+from collections import deque
 n = int(input())
 
-visited = [[False]*(n+1) for _ in range(n+1)]
-
+# 퀸이 놓여진 위치를 저장할 리스트
+q = deque()
 result = 0
-dx = []
-dy = []
-def dfs(x, y, count, visited):
-    global n, result
-    if count == n:
+
+def dfs(x):
+    global result, q
+    if x == n:
         result += 1
         return
-    if not visited[x][y]:
-        #주변방문처리
-        for i in range(1, n+1):
-            visited[x][i] = True
-            visited[i][y] = True
-        for
 
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                dfs(i, j, count+1, visited)
+    for y in range(n):
+        if ok(x, y):
+            print(x, y)
+            q.append((x, y))
+            dfs(x+1)
+            q.pop()
+def ok(x, y):
+    global q
+    for i in q:
+        if x == i[0]:
+            return False
+        if abs(i[0] - x) == abs(i[1] - y):
+            return False
+    return True
 
-
+dfs(0)
+print(result)
